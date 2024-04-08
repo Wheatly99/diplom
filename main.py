43889947c4a45e20f52d8837bb28e06cfd930cb5
Wheatly99 <99mmarkin@gmail.com>
2024-04-08 19:37:39 +0500

    Add lab5

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lab1 import final_score_lab1
 from lab2 import final_score_lab2
 from lab3 import final_score_lab3
 from lab4 import final_score_lab4
+from lab5 import final_score_lab5
 from utils import remove_not_empty_dir
 import os
 
@@ -22,45 +23,25 @@ if __name__ == '__main__':
     lab4 = pd.read_excel(file_name, sheet_name='lab4')
     lab5 = pd.read_excel(file_name, sheet_name='lab5')
 
-    if ~lab1.empty:
-        lab1 = lab1.merge(teams[teams.lab1_score.isnull()], left_on='index', right_on='link_index', how='inner')[
-            ['index', 'link']].drop_duplicates()
-        lab1['lab1_score'] = lab1.link.apply(lambda x: final_score_lab1(x))
-        teams = teams.merge(lab1, left_on='link_index', right_on='index', how='left')
+    list_labs = [[lab1, final_score_lab1],
+                 [lab2, final_score_lab2],
+                 [lab3, final_score_lab3],
+                 [lab4, final_score_lab4],
+                 [lab5, final_score_lab5]]
 
-        teams['lab1_score'] = teams.lab1_score_x.combine_first(teams.lab1_score_y)
+    def add_score(lab, lab_score, final_score_lab, teams):
+        lab = lab.merge(teams[teams[lab_score].isnull()], left_on='index', right_on='link_index', how='inner')[
+            ['index', 'link']].drop_duplicates()
+        lab[lab_score] = lab.link.apply(lambda x: final_score_lab(x))
+        teams = teams.merge(lab, left_on='link_index', right_on='index', how='left')
+        teams[lab_score] = teams[lab_score + '_x'].combine_first(teams[lab_score + '_y'])
         teams = teams[
             ['group', 'full_name', 'link_index', 'lab1_score', 'lab2_score', 'lab3_score', 'lab4_score', 'lab5_score']]
+        return teams
 
-    if ~lab2.empty:
-        lab2 = lab2.merge(teams[teams.lab2_score.isnull()], left_on='index', right_on='link_index', how='inner')[
-            ['index', 'link']].drop_duplicates()
-        lab2['lab2_score'] = lab2.link.apply(lambda x: final_score_lab2(x))
-        teams = teams.merge(lab2, left_on='link_index', right_on='index', how='left')
-
-        teams['lab2_score'] = teams.lab2_score_x.combine_first(teams.lab2_score_y)
-        teams = teams[
-            ['group', 'full_name', 'link_index', 'lab1_score', 'lab2_score', 'lab3_score', 'lab4_score', 'lab5_score']]
-
-    if ~lab3.empty:
-        lab3 = lab3.merge(teams[teams.lab3_score.isnull()], left_on='index', right_on='link_index', how='inner')[
-            ['index', 'link']].drop_duplicates()
-        lab3['lab3_score'] = lab3.link.apply(lambda x: final_score_lab3(x))
-        teams = teams.merge(lab3, left_on='link_index', right_on='index', how='left')
-
-        teams['lab3_score'] = teams.lab3_score_x.combine_first(teams.lab3_score_y)
-        teams = teams[
-            ['group', 'full_name', 'link_index', 'lab1_score', 'lab2_score', 'lab3_score', 'lab4_score', 'lab5_score']]
-
-    if ~lab4.empty:
-        lab4 = lab4.merge(teams[teams.lab4_score.isnull()], left_on='index', right_on='link_index', how='inner')[
-            ['index', 'link']].drop_duplicates()
-        lab4['lab4_score'] = lab4.link.apply(lambda x: final_score_lab4(x))
-        teams = teams.merge(lab4, left_on='link_index', right_on='index', how='left')
-
-        teams['lab4_score'] = teams.lab4_score_x.combine_first(teams.lab4_score_y)
-        teams = teams[
-            ['group', 'full_name', 'link_index', 'lab1_score', 'lab2_score', 'lab3_score', 'lab4_score', 'lab5_score']]
+    for number, lab in enumerate(list_labs):
+        if ~lab[0].empty:
+            teams = add_score(lab[0], f'lab{number + 1}_score', lab[1], teams)
 
     with pd.ExcelWriter(file_name, engine='openpyxl', mode="a", if_sheet_exists="replace") as writer:
         teams.to_excel(writer, "teams", index=False)
